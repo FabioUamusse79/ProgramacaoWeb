5c12e850bc3ac75df7f138ed1802fc6a1b3c98d0
FabioUamusse79 <fabiouamusse92@gmail.com>
2026-09-02 10:17:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 alert("Bem-vindo ao meu site!");
+const formulario = document.getElementById("formulario");
+const resultado = document.getElementById("resultado");
 
+formulario.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+
+    resultado.textContent =
+        "Formulário enviado com sucesso! Obrigado, " + nome + ".";
+
+    console.log("Nome:", nome);
+    console.log("E-mail:", email);
+
+    formulario.reset();
+});
